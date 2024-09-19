452201baa59184f88f3203e2699c6cf6bfc6e9fb
D3ller <corentin.nelhomme@gmail.com>
2024-09-19 10:43:30 +0200

    del. icon from vue, remove about routes feat. add font support

--- kickstarter/src/App.vue
+++ kickstarter/src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
 
   <RouterView />
 </template>

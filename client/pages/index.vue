5778494fe3ee4c859235d1f81578603fe4164693
D3ller <corentin.nelhomme@gmail.com>
2024-10-15 16:19:52 +0200

    update index.vue

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,57 @@
 <script setup>
 
+import {io} from 'socket.io-client'
+import HeaderBanner from "~/components/global/section/headerBanner.vue";
+import Piment from "~/components/global/section/piment.vue";
+import FAQ from "~/components/global/section/FAQ.vue";
+
+const router =useRouter()
+
+const socket = io({
+  path: '/api/ws',
+  addTrailingSlash: false,
+})
+
+let text = ref('')
+let created = ref(false)
+
+let createRoom = () => {
+  socket.emit('create-room', text.value)
+}
+
+let joinRoom = () => {
+  socket.emit('join-room', text.value)
+}
+
+socket.on('room-created', (message) => {
+  created.value = true
+router.push('/salle/'+message)
+})
 </script>
 
 <template>
-  sssss
+
+  <div id="page_home">
+    <header-banner></header-banner>
+    <piment></piment>
+    <f-a-q></f-a-q>
+
+  </div>
+  <!--  <div>-->
+  <!--    <Button >Se connecter</Button>-->
+  <!--    <Button variant="link">Se connecter</Button>-->
+  <!--    <Button variant="light">Se connecter</Button>-->
+  <!--    <Button variant="dark">Se connecter</Button>-->
+  <!--  </div>-->
+
+  <!--  <game-card text="j'ai des margiela a mes  pieds toi t'en a pas lalalilalair" variant=""></game-card>-->
+
+  <!--  <input v-model="text">-->
+  <!--  <Button @click="createRoom()">Crée une room</Button>-->
+  <!--  <Button @click="joinRoom()">Join room</Button>-->
+  <!--  <Button v-if="created" @click="launchParty">Lancer la partie</Button>-->
+  <!--  </div>-->
+
 </template>
 
 <style scoped>
